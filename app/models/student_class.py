c4230bd8fa95e903598004cd429164dfc0c8394a
sause668 <soulsitescoding@zohomail.com>
2024-12-02 20:37:45 -0500

    versions complete

--- app/models/student_class.py
+++ app/models/student_class.py
@@ -9,8 +9,8 @@ class StudentClass(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     # id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('students.id')), nullable=False)
-    class_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('classes.id')), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('students.id')), nullable=False, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('classes.id')), nullable=False, primary_key=True)
 
     def to_dict(self):
         return {
