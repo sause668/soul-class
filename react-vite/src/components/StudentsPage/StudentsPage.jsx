5bf82525955a05c81ddadc4b8ef5e7107042b388
sause668 <soulsitescoding@zohomail.com>
2025-02-27 17:10:27 -0500

    student search table

--- react-vite/src/components/StudentsPage/StudentsPage.jsx
+++ react-vite/src/components/StudentsPage/StudentsPage.jsx
@@ -25,13 +25,50 @@ export default function StudentsPage() {
   return (
     <>
       {isLoaded && (
-        <div id="studentsCon">
-          {students.map((student, index) => (
-            <div key={`studentInfo${index}`}>
-                <h3 onClick={()=>handleNavStudent(student.id)} >{student.first_name} {student.last_name}</h3>
+        <div id='studentsCon'>
+            <div id="HeaderConSS">
+                <div id="titleConSS">
+                    <h1 id="titleSS">Student Search</h1>
+                </div>
+                <div id="searchConSS">
+                    <input type="text" name="studentSearch" id="searchInputSS" placeholder="Search" />
+                </div>
             </div>
-          ))}
+
+            <div id="tableConSS">
+                <table id="tableSS">
+                    <thead id="tableHeadSS">
+                        <tr className="tableHeadRowCC">
+                            <td className="tableCellSS tableHeadCellSS">Last Name</td>
+                            <td className="tableCellSS tableHeadCellSS">First Name</td>
+                            <td className="tableCellSS tableHeadCellSS">Grade</td>
+                        </tr>
+                    </thead>
+                    <tbody id="tableBodySS">
+                        {students.map((student, index) => (
+                            <tr 
+                                className="tableBodyRowCC" 
+                                key={`studentSearchTable${index}`}
+                                onClick={()=>handleNavStudent(student.id)}
+                            >
+                                <td className="tableCellSS" >{student.last_name}</td>
+                                <td className="tableCellSS" >{student.first_name}</td>
+                                <td className="tableCellSS" >{student.grade}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            
+            {/* <div id="studentsCon">
+            {students.map((student, index) => (
+                <div key={`studentInfo${index}`}>
+                    <h3 onClick={()=>handleNavStudent(student.id)} >{student.first_name} {student.last_name}</h3>
+                </div>
+            ))}
+            </div> */}
         </div>
+        
       )}
     </>
   );
