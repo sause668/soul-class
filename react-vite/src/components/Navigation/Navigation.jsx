fd53807d18a64ce6e270d3c01ebf26d4b53b5425
sause668 <soulsitescoding@zohomail.com>
2026-02-11 16:57:02 -0500

    groups

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import StudentSearch from "./StudentSearch";
 import { IoChevronBack } from "react-icons/io5";
 
 import "./Navigation.css";
@@ -11,21 +12,26 @@ function Navigation() {
 
   return (
     <div id="navCon">
+      {/* <div id="navLeftCon" className="flex justify-start items-center gap-2"> */}
       {(currentPath === '/') ? (
-        <div id="navLogoCon">
-          <img src="/imgs/logo.png" alt="logo" id="navLogo"/>
+        <div id="navLogoCon" className="w-1/3">
+          {/* <img src="/imgs/logo.png" alt="logo" id="navLogo"/> */}
         </div>
       ):(
-        <div id="backButtonCon">
+        <div id="backButtonCon" className="w-1/3">
           <button id="backButton" type="button" onClick={()=>nav(-1)}>
             <IoChevronBack id="backButtonIcon" />
           </button>
         </div>
       )}
-      <div id={(currentPath === '/') ? 'navTitleCon':''} >
+      <div id={(currentPath === '/') ? 'navTitleCon':''} className="w-1/3">
         <h1 id="navTitle" className="text-5xl font-bold cursor-pointer p-2 pb-3" onClick={()=>nav('/')}>Soul Academy</h1>
       </div>
-      <ProfileButton/>
+      {/* </div> */}
+      <div id="navRightCon" className="">
+        <StudentSearch />
+        <ProfileButton/>
+      </div>
     </div>
   );
 }
